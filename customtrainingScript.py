4c4f3cee8dbf0792b50d0f49dcf342978210b719
hassan kazemi tehrani <shayanthrn@gmail.com>
2022-01-04 22:57:36 +0330

    add cls

--- customtrainingScript.py
+++ customtrainingScript.py
@@ -1,15 +1,21 @@
 import subprocess
 import sys
+import os
+
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 print("installing dependencies...")
-packages = ['pyworld','resemblyzer','wandb','librosa','soundfile']
+packages = ['pyworld','resemblyzer','wandb','librosa','soundfile','requests']
 for package in packages:
     try:
         install(package)
     except:
         pass
+    
+clearConsole()
 print("dependencies installed succesfully!")
